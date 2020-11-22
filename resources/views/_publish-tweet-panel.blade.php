bf049c0dd8bc0871561edc40af21305af364810d
peakacode <peakacode@gmail.com>
2020-11-22 22:30:46 +0800

    57 Make the Timeline Dynamic
    My Laracast Code Along Project Journey

--- resources/views/_publish-tweet-panel.blade.php
+++ resources/views/_publish-tweet-panel.blade.php
@@ -1,21 +1,30 @@
 <div class="border border-blue-400 rounded-lg px-8 py-6 mb-8">
-                <form action="">
-                    <textarea 
-                    name="body" 
-                    class="w-full"
-                    placeholder="Watch up Duck!"
-                    ></textarea>
+        <form method="POST" action="/tweets">
+            @csrf
+            <textarea 
+            name="body" 
+            class="w-full"
+            placeholder="Watch up Duck!"
+            
+            ></textarea>
 
-                    <hr class="my-4">
+            <hr class="my-4">
 
-                    <footer class="flex justify-between">
-                        <img 
-                            src="https://i.pravatar.cc/40" 
-                            alt=""
-                            class="rounded-full mr-2"
-                        >
-                        <button type="submit" class="bg-blue-500 rounded-lg shadow py-1 px-2 text-white">Tweet-a-roo!</button>
-                    </footer>
-                </form>
-            </div>  
-        <div>
+            <footer class="flex justify-between">
+                <img 
+                    src="{{ auth()->user()->avatar }}" 
+                    alt="User Avatar"
+                    class="rounded-full mr-2"
+                >
+                <button 
+                    type="submit" 
+                    class="bg-blue-500 rounded-lg shadow py-1 px-2 text-white">
+                    Tweet-a-roo!
+                </button>
+            </footer>
+        </form>
+        @error('body')
+            <p class="text-red-500 text-sm mt-2">{{ $message }}</p>
+        @enderror
+    </div>  
+<div>
